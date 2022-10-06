b32571efea1e3e3a6558938150f246ccb1ffc0ab
DSII-SS <DStevens@skillstorm.com>
2022-10-06 10:27:02 -0400

    Main Structure and index setup
    //npm init -y
    //npm i express mongoose cors
    //"start": "node index.js" in package json scripts
    //index.js setup

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+//npm init -y
+//npm i express mongoose cors dotenv
+
+//Requirements
+const express = require('express');
+const mongoose = require('mongoose')
+const cors = require('cors')
+require('dotenv').config();  
+
+const app = express();
+app.use(cors()); 
+app.use(express.json());
+
+//Routes
+//app.use('/warehouse',WarehouseRouter)
+
+
+const ConnectToMongo = async () => {
+    try{
+        await mongoose.connect(process.env.MONGO_URI);  
+        console.log(`Connected to MongoDB`);
+    }
+    catch (err) {
+        //Runs if it fails to connect to MongoDB
+        console.error(err);
+        process.exit(1);  //Instantly kills the server
+    }
+}
+
+//Default to port 8080 if something else goes wrong
+app.listen(process.env.PORT || 8080, () =>{
+    //Callback runs right when the app starts
+    console.log(`Listening on port ${process.env.PORT || 8080}`);
+})
+
+
+
+ConnectToMongo();
+//npm start
